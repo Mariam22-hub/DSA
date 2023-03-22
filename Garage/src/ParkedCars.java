09164b2c415f3413c1b105a9248d21436caa0fb8
Mariam Elmoazen <11410120200528@stud.cu.edu.eg>
2023-03-22 18:02:31 +0200

    Delete Garage directory

--- Garage/src/ParkedCars.java
+++
@@ -1,9 +0,0 @@
-import java.util.ArrayList;
-
-public class ParkedCars {
-	public static ArrayList<Vehicle> parkedCars = new ArrayList<>();
-	
-	public ArrayList<Vehicle> parked(){
-		return parkedCars;
-	}
-}
